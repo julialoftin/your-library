803d5a27a1ede77c863ddd5420b6f8c8f06d7055
Julia Loftin <julia.loftin7@gmail.com>
2023-12-03 10:00:35 -0600

    creates book class and template for displaying book class

---
+++ src/main/java/com/example/models/Book.java
@@ -0,0 +1,59 @@
+package com.example.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String author;
+    private String title;
+    private Integer isbn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
